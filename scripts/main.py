7219dd231e2648b90402a04af30569961cdca456
Simen Uthaug <sut020@uib.no>
2025-05-25 02:05:06 +0200

    Clean .gitignore and remove unneeded folders from tracking

--- scripts/main.py
+++ scripts/main.py
@@ -15,6 +15,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+def root():
+    return {"status": "API running"}
+
 @app.post("/clean-csv")
 async def clean_csv(file: UploadFile = File(...)):
     contents = await file.read()
