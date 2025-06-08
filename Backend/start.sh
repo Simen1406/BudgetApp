7923a63b1b90e523a246592624efb7395f03bdd3
Simen Uthaug <sut020@uib.no>
2025-06-08 14:16:25 +0200

    fixed type.

--- Backend/start.sh
+++ Backend/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gunicorn -k uvicorn.workers.UvicornWorker main:app --chdir backend --bind 0.0.0.0:$PORT
+gunicorn -k uvicorn.workers.UvicornWorker main:app --chdir Backend --bind 0.0.0.0:$PORT
